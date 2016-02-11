9ed4864f2bb566711e1f5d28c3d6930e29e480a4
garreth398 <garreth.flynn24@mail.dcu.ie>
2016-02-11 10:21:21 +0000

    Create MHT.java

---
+++ MHT.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.net.URL;
+import java.io.Serializable;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class MHT extends JFrame implements ActionListener
+{
+	//Basic variable needed.
+	JPanel panel;
+	
+	public MHT()
+	{
+		super("Mental Health Tips");
+		//Set variables.
+		panel = new JPanel();
+		setSize(180*3,180*3);
+		setResizable(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+		
+		panel.setLayout(new BorderLayout());	 
+		add(panel);
+		setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+
+	}
+}
