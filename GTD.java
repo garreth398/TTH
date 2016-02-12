94d0a045d52f8685c3a9f18dd01f032cc83d5b01
grabartley <grabartley@gmail.com>
2016-02-12 21:36:53 +0000

    Update GTD.java
    Added a text field and button

--- GTD.java
+++ GTD.java
@@ -12,25 +12,42 @@ import java.io.FileNotFoundException;
 public class GTD extends JFrame implements ActionListener
 {
 	//Basic variable needed.
+    String thought;
 	JPanel panel;
+    JButton addButton;
+    JTextField textField;
+    JLabel label;
 	
 	public GTD()
 	{
 		super("Good Thoughts Diary");
 		//Set variables.
 		panel = new JPanel();
+        addButton = new JButton("Add");
+        textField = new JTextField(20);
+        label = new JLabel("");
 		setSize(180*3,180*3);
 		setResizable(true);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
-		
-		panel.setLayout(new BorderLayout());	 
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new FlowLayout());
+		//panel.setLayout(new BorderLayout());
+        
 		add(panel);
+        add(textField);
+        add(addButton);
+        add(label, FlowLayout.CENTER);
+        addButton.addActionListener(this);
 		setVisible(true);
 	}
 	
+    @Override
 	public void actionPerformed(ActionEvent e)
 	{
-
+        thought = textField.getText();
+        textField.setText("");
+        textField.requestFocus();
+        label.setText(thought);
+        thought = "";
 	}
 }
