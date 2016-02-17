b825391ac53bd1be9a7ff5da266e4b5f1f3cdbe0
grabartley <grabartley@gmail.com>
2016-02-17 14:15:06 +0000

    Update HQ.java

--- HQ.java
+++ HQ.java
@@ -7,30 +7,73 @@ import java.net.URL;
 import java.io.Serializable;
 import java.io.File;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Random;
 
 public class HQ extends JFrame implements ActionListener
 {
 	//Basic variable needed.
 	JPanel panel;
+	JButton refresh;
+	JLabel quote;
+	ArrayList<String> quotes;
+	File file;
+	Scanner scan;
+	int len;
+	int randQuote;
 	
 	public HQ()
 	{
 		super("Happy Quotes");
 		//Set variables.
 		panel = new JPanel();
+		refresh = new JButton("Refresh");
+		
 		setSize(180*3,180*3);
 		setResizable(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+		setLayout(new FlowLayout());
+		panel.setLayout(new BorderLayout());
 		
-		panel.setLayout(new BorderLayout());	 
+		quotes = new ArrayList<>();
+		file = new File("hq.txt");
+        try {
+            scan = new Scanner(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+		while(scan.hasNext())
+		{
+			String s = scan.nextLine();
+			quotes.add(s);
+		}
+		
+		quotes.trimToSize();
+		len = quotes.size();
+		
+		randQuote = (int)(Math.random() * len);
+		quote = new JLabel("");
+		quote.setText(quotes.get(randQuote));
+		
+		refresh.addActionListener(this);
+		
+		panel.setLayout(new BorderLayout());
 		add(panel);
+		add(refresh, BorderLayout.CENTER);
+		add(quote, BorderLayout.CENTER);
 		setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-
+		if(e.getSource() == refresh)
+		{
+			randQuote = (int)(Math.random() * len);
+			quote.setText("");
+			quote.setText(quotes.get(randQuote));
+			add(quote);
+		}
 	}
 }
